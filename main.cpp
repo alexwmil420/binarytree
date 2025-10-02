cc3a8e3dd83f0ca3a1f1dc02e0922ad8a878edb2
alexmilian <2022156742@ub.edu.bz>
2025-10-02 05:00:46 +0000

    Add inorder traversal

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Each node will store a string as its data
+struct BinaryTreeNode {
+    string data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(const string& value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+// Only the order of the recursive calls change for pre-order and post-order
+void inorder(BinaryTreeNode* root) {
+    if (root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+
+
